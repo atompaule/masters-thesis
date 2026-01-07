fix: Train in full float32 precision to avoid precision problems

src/hrpo/eval_grpo_gsm8k.py
@@ -42,7 +42,7 @@ def evaluate_model(
 
     model = Qwen2ForCausalLM.from_pretrained(
         model_path,
-        torch_dtype="auto",
+        torch_dtype=torch.float32,
         device_map="auto",
     )
     model.answer_start = ANSWER_START

src/hrpo/eval_hrpo_gsm8k.py
@@ -48,7 +48,7 @@ def evaluate_model(
 
     model = HRPOQwen2ForCausalLM.from_pretrained(
         model_path,
-        torch_dtype="auto",
+        torch_dtype=torch.float32,
         device_map="auto",
     )
     model.answer_start = ANSWER_START

src/hrpo/train_grpo_gsm8k.py
@@ -4,6 +4,8 @@ import os
 import sys
 from datetime import datetime
 
+import torch
+
 project_root = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 )
@@ -64,7 +66,7 @@ def main(args):
 
     model = Qwen2ForCausalLM.from_pretrained(
         args.model_name,
-        torch_dtype="auto",
+        torch_dtype=torch.float32,
         device_map="auto",
     )
     model.answer_start = ANSWER_START
@@ -90,7 +92,7 @@ def main(args):
     )
 
     training_args = GRPOConfig(
-        bf16=True,
+        bf16=False,
         use_vllm=False,
         learning_rate=args.lr,
         beta=0.0,  # args.beta,
@@ -112,6 +114,7 @@ def main(args):
         save_steps=250,
         save_total_limit=3,
         report_to="wandb",
+        run_name=run_name,
         output_dir=exp_name,
     )
 
@@ -146,7 +149,7 @@ if __name__ == "__main__":
     parser.add_argument("--weight_decay", type=float, default=0.1)
     parser.add_argument("--warmup_ratio", type=float, default=0.1)
     parser.add_argument("--lr_scheduler_type", type=str, default="cosine")
-    parser.add_argument("--optimizer", type=str, default="paged_adamw_8bit")
+    parser.add_argument("--optimizer", type=str, default="adamw_torch")
     parser.add_argument("--max_grad_norm", type=float, default=0.1)
 
     parser.add_argument("--group_size", type=int, default=4)

src/hrpo/train_hrpo_gsm8k.py
@@ -79,7 +79,7 @@ def main(args):
 
     model = HRPOQwen2ForCausalLM.from_pretrained(
         args.model_name,
-        torch_dtype="auto",
+        torch_dtype=torch.float32,
         device_map="auto",
     )
     model.answer_start = ANSWER_START
@@ -120,7 +120,7 @@ def main(args):
     # check_trainable_status(model)
 
     training_args = GRPOConfig(
-        bf16=True,
+        bf16=False,
         use_vllm=False,
         learning_rate=args.lr,
         beta=0.0,  # args.beta TODO: fix weird latent gate params being nan in self.accelerator.unwrap_model(self.model).disable_adapter()
@@ -178,12 +178,12 @@ if __name__ == "__main__":
     parser.add_argument("--beta", type=float, default=0.005)
     parser.add_argument("--residual_r_min", type=float, default=0.99)
     parser.add_argument("--residual_r_max", type=float, default=0.999)
-    parser.add_argument("--lr_residual_gate", type=float, default=1e-2)
-    parser.add_argument("--lr_residual_Lambda", type=float, default=1e-1)
+    parser.add_argument("--lr_residual_gate", type=float, default=1e-4)
+    parser.add_argument("--lr_residual_Lambda", type=float, default=1e-3)
     parser.add_argument("--weight_decay", type=float, default=0.1)
     parser.add_argument("--warmup_ratio", type=float, default=0.1)
     parser.add_argument("--lr_scheduler_type", type=str, default="cosine")
-    parser.add_argument("--optimizer", type=str, default="paged_adamw_8bit")
+    parser.add_argument("--optimizer", type=str, default="adamw_torch")
     parser.add_argument("--max_grad_norm", type=float, default=0.1)
 
     parser.add_argument("--group_size", type=int, default=4)